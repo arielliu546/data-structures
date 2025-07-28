f0f008145eecb022acbab47a988080c793fba630
Ariel <arielliu546@gmail.com>
2025-07-28 11:46:21 +0800

    TimeAList

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -17,11 +17,51 @@ public class TimeAList {
         }
     }
 
+    private static class TimeData {
+        int N;
+        double time;
+        int opCount;
+        public TimeData(int n, double t, int o) {
+            N = n;
+            time = t;
+            opCount = o;
+        }
+    }
+
+    private static TimeData timeOps(int N) {
+        AList<Integer> al = new AList<Integer>();
+        int opCount = 0;
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < N; i ++) {
+            al.addLast(1);
+            opCount++;
+        }
+        double timeInSeconds = sw.elapsedTime();
+        return new TimeData(N, timeInSeconds, opCount);
+    }
+
+    private static void processTimeData(int N, AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        TimeData data = timeOps(N);
+        Ns.addLast(data.N);
+        times.addLast(data.time);
+        opCounts.addLast(data.opCount);
+    }
+
     public static void main(String[] args) {
         timeAListConstruction();
     }
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<Integer>();
+        AList<Double> times = new AList<Double>();
+        AList<Integer> opCounts = new AList<Integer>();
+
+        int[] list = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 10000000};
+        for (int j : list) {
+            processTimeData(j, Ns, times, opCounts);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 }
